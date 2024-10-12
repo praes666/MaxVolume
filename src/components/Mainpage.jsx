e22732d4d5ae7cc7410349dbcae8e7b1e57993e5
praes666 <praesvip@mail.ru>
2024-10-12 13:22:27 +0300

    add mainpage

---
+++ src/components/Mainpage.jsx
@@ -0,0 +1,11 @@
+export default function _mainpage(){
+    return(
+        <div className="centered">
+            <div className="ad">
+                <a href="https://службапоконтракту.рф/">
+                    <img src="sys_img/svo.jpg" alt=""/>
+                </a>
+            </div>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import { createRoot } from 'react-dom/client'
+
 import _header from './components/Header.jsx'
+import _mainpage from './components/Mainpage.jsx'
 import _player from './components/Player.jsx'
 
 import './styles/const.css'
 import './styles/header.css'
 import './styles/player.css'
+import './styles/mainpage.css'
 
 createRoot(document.getElementById('root')).render(
     <div className='full'>
         <_header/>
-        
+            <_mainpage />
         <_player/>
     </div>
 )
